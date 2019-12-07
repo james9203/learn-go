01aa71fb920fd4f6dd05b8d8ce1e48756ad9f022
james <james9203@163.com>
2019-12-07 12:51:40 +0800

    variable

---
+++ Variable/variable_3.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type (
+	byte1  int8
+	rune   int32
+	ByteSize int64
+	bo   bool
+)
+func main()  {
+	var a  byte1
+	var b rune
+	var c ByteSize
+	var d bo
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	//var e,f,g,h int = 1,2,3,4
+	e,f,g,h := 1,2,3,4
+	fmt.Println(e,f,g,h)
+}
