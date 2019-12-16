003fa40f2687bd6ba2dcc614183ad28fccc8986c
luozhangqiao <james9203@126.com>
2019-12-16 19:11:47 +0800

    html

--- Html/html.go
+++ Html/html.go
@@ -5,11 +5,11 @@ import (
 	"html"
 )
 
-func main()  {
-	 s := `"Fran & Freddie's Diner" <tasty@example.com>`
-	 es := html.EscapeString(s)
-	 fmt.Println(es)
-	 us := html.UnescapeString(es)
+func main() {
+	s := `"Fran & Freddie's Diner" <tasty@example.com>`
+	es := html.EscapeString(s)
+	fmt.Println(es)
+	us := html.UnescapeString(es)
 	fmt.Println(us)
 
 }
