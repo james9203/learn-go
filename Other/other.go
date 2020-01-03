b856a1a2fd635f6ed48b9a6c215a750a7b9c5c36
luozhangqiao <james9203@126.com>
2020-01-03 12:06:23 +0800

    orther

--- Other/other.go
+++ Other/other.go
@@ -1,1 +1,18 @@
-package Other
+package main
+
+import "fmt"
+
+func doubleScore(source float32) (score float32) {
+	defer func() {
+		if score < 1 || score >= 100 {
+			score = source
+		}
+	}()
+	return source * 2
+}
+
+func main() {
+	fmt.Println(doubleScore(0))
+	fmt.Println(doubleScore(20.0))
+	fmt.Println(doubleScore(50.0))
+}
