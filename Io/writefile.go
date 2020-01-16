136a713dfcc9dcaa5436af264f7e3619974aca75
luozhangqiao <james9203@126.com>
2020-01-16 16:16:07 +0800

    writefile

--- Io/writefile.go
+++ Io/writefile.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func main() {
+func writestring() {
 	f, err := os.Create("ctest.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -23,4 +23,33 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+}
+func writebytes() {
+	f, err := os.Create("bytes")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	d2 := []byte{104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100}
+	n2, err := f.Write(d2)
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	fmt.Println(n2, "bytes written successfully")
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func main() {
+	fmt.Println("-------------------------------------")
+	writestring()
+	fmt.Println("-------------------------------------")
+	writebytes()
+	fmt.Println("-------------------------------------")
+
 }
