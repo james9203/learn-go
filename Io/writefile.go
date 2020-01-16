5eae512e813572a38566eea43273e58d3e1cecbf
luozhangqiao <james9203@126.com>
2020-01-16 11:52:48 +0800

    write

--- Io/writefile.go
+++ Io/writefile.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"os"
+	"fmt"
+)
+
+func main() {
+	f, err := os.Create("d:/ctest.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	l, err := f.WriteString("Hello World")
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	fmt.Println(l, "bytes written successfully")
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
