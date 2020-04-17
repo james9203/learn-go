6d10c6ef48e222b89dce5dc606eebf2379caaecc
luozhangqiao <james9203@126.com>
2020-04-17 17:49:22 +0800

    change

--- Cron/cron.go
+++ Cron/cron.go
@@ -8,15 +8,15 @@ import (
 func main() {
 	i := 0
 	c := cron.New()
-	spec := "*/5 * * * * *"
+	spec := "0/1 * * * * * "
 	c.AddFunc(spec, func() {
 		i++
 		log.Println("cron running:", i)
 	})
-	c.AddFunc("@every 1h1m", func() {
-		i++
-		log.Println("cron running:", i)
-	})
+	//c.AddFunc("@every 1h1m", func() {
+	//	i++
+	//	log.Println("cron running:", i)
+	//})
 	c.Start()
 	select {
 
